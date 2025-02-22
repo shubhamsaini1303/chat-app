999d3f13f816153cfc7cde89ea519757f6a30a88
shubhamsaini1303 <144662519+shubhamsaini1303@users.noreply.github.com>
2025-02-22 20:32:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 // Ensure correct CORS configuration
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Replace with your frontend origin
+    origin:true, // Replace with your frontend origin
     methods: ["GET", "POST"],
     credentials: true,
   },
